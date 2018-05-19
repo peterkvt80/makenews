eeec58607ad6dfcd6ce72fee57ec017b66e94ec3
N. Dane <avrovulcanxh607@gmail.com>
2018-05-19 23:56:28 +0100

    Update tvlistingsreader.php

--- tvlistingsreader.php
+++ tvlistingsreader.php
@@ -1,7 +1,7 @@
 <?php
-$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc1_n_ireland&all");		
+$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc1&all");		
 file_put_contents("BBC10.html",$str);
-$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc2_n_ireland&all");		
+$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc2&all");		
 file_put_contents("BBC20.html",$str);
 //$str = file_get_contents("http://www.ontvtonight.co.uk/guide/listings/channel/69036087/utv.html");		
 //file_put_contents("UTV0.html",$str);
@@ -9,9 +9,9 @@ $str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=ch4&all");
 file_put_contents("C40.html",$str);
 $str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=five&all");		
 file_put_contents("C50.html",$str);
-$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc1_n_ireland&all&day=1");		
+$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc1&all&day=1");		
 file_put_contents("BBC13.html",$str);
-$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc2_n_ireland&all&day=1");		
+$str = file_get_contents("http://www.bleb.org/tv/channel.html?ch=bbc2&all&day=1");		
 file_put_contents("BBC23.html",$str);
 //$str = file_get_contents("http://www.ontvtonight.co.uk/guide/listings/channel/69036087/utv.html");
 //file_put_contents("UTV3.html",$str);	// Can't find tomorrow's listings for UTV, Doesn't work anyway.
