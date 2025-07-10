4ff741a39fb2d2f952da7a1505db9a40d7bbdadb
Max de Vos <97400699+Henkdetenk12345@users.noreply.github.com>
2025-07-10 09:49:45 +0200

    Changed basic script to work with modern bbc website

--- newsreader.php
+++ newsreader.php
@@ -1,52 +1,25 @@
 <?php
-include "simple_html_dom.php";
-$rssfeed="http://feeds.bbci.co.uk/news/uk/rss.xml?edition=uk";	// BBC UK stories
+// This grabs the rss feed, extracts the story links and copies each page.
+// Best not to run this too often to reduce the extra load on the BBC website!
+$rssfeed="http://feeds.bbci.co.uk/news/rss.xml";	// BBC top stories
+//$rssfeed="rss.xml";	// Debug offline
 $rawFeed = file_get_contents($rssfeed);
 $xml = new SimpleXmlElement($rawFeed);
+echo $xml->channel->title;
+echo $xml->asXML;
 $count=0;
 foreach($xml->channel->item as $chan) {
-	// Don't want video/sport stories. They don't render too well on teletext
-	if (strncmp($chan->title,"VIDEO:",6)) 
-	if (strncmp($chan->link,"http://www.bbc.co.uk/sport/",26))
+	// Don't want video stories. They don't render too well on teletext
+	if (strncmp($chan->title,"VIDEO:",6))
 	{
-		$url=$chan->link; 
-		$str = file_get_html($url);
-		$title=$str->find("link[rel=canonical]");
-		$title=substr ($title[0],35);
-		$title=substr($title, 0, strpos( $title, '"'));
-		echo $title."\n";
-		if (!strncmp($title,"www.bbc.co.uk/news/av/",21))
-		{
-			continue 1;
-		}
-		echo $chan->title."\n";
+		$url=$chan->link;
+		echo $url."\n";
+		echo $chan->title."\n"; 
+		$str = file_get_contents($url);		
 		file_put_contents("page$count.html",$str);	// Save each as Page<x>.html
+		//echo "$str\n";
 		$count++;
-		if ($count>10) break;	// Stop after we get the pages that we want
-	}
-} 
-$rssfeed="http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk";	// BBC world stories
-$rawFeed = file_get_contents($rssfeed);
-$xml = new SimpleXmlElement($rawFeed);
-foreach($xml->channel->item as $chan) {
-	// Don't want video/sport stories. They don't render too well on teletext
-	if (strncmp($chan->title,"VIDEO:",6)) 
-    if (strncmp($chan->link,"http://www.bbc.co.uk/sport/",25))
-	{
-		$url=$chan->link; 
-		$str = file_get_html($url);
-		$title=$str->find("link[rel=canonical]");
-		$title=substr ($title[0],35);
-		$title=substr($title, 0, strpos( $title, '"'));
-		echo $title."\n";
-		if (!strncmp($title,"www.bbc.co.uk/news/av/",21))
-		{
-			continue 1;
-		}
-		echo $chan->title."\n";
-		file_put_contents("page$count.html",$str);	// Save each as Page<x>.html
-		$count++;
-		if ($count>20) exit;	// Stop after we get the pages that we want
+		if ($count>30) exit;	// Stop after we get the pages that we want
 	}
 } 
 ?>
